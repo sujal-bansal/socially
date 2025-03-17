74441bf9d9123d80d7c22e3be5c9a7cfd6e1b099
sujal-bansal <bansalsujal2021@gmail.com>
2025-03-18 00:10:53 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>Home Page Content</h1>
